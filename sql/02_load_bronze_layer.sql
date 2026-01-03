e5aaf0901ad3533960f7b9defd5f4b9e00cbfeb1
EdivanCarvalho1 <edivanjuniorfreitas@gmail.com>
2026-01-03 14:54:33 -0300

    add: procedure load_bronze_layer

--- sql/02_load_bronze_layer.sql
+++ sql/02_load_bronze_layer.sql
@@ -1,49 +1,63 @@
-TRUNCATE TABLE bronze.crm_cust_info;
-COPY bronze.crm_cust_info
-FROM 'D:\projects\crm_erp_datawarehouse\datasets\source_crm\cust_info.csv'
-WITH (
-    FORMAT csv,
-    HEADER true,
-    DELIMITER ','
-);
-TRUNCATE TABLE bronze.crm_prd_info;
-COPY bronze.crm_prd_info
-FROM 'D:\projects\crm_erp_datawarehouse\datasets\source_crm\prd_info.csv'
-WITH (
-    FORMAT csv,
-    HEADER true,
-    DELIMITER ','
-);
-TRUNCATE TABLE bronze.crm_sales_details;
-COPY bronze.crm_sales_details
-FROM 'D:\projects\crm_erp_datawarehouse\datasets\source_crm\sales_details.csv'
-WITH (
-    FORMAT csv,
-    HEADER true,
-    DELIMITER ','
-);
-TRUNCATE TABLE bronze.erp_cust_az12;
-COPY bronze.erp_cust_az12
-FROM 'D:\projects\crm_erp_datawarehouse\datasets\source_erp\CUST_AZ12.csv'
-WITH (
-    FORMAT csv,
-    HEADER true,
-    DELIMITER ','
-);
-TRUNCATE TABLE bronze.erp_loc_a101;
-COPY bronze.erp_loc_a101
-FROM 'D:\projects\crm_erp_datawarehouse\datasets\source_erp\LOC_A101.csv'
-WITH (
-    FORMAT csv,
-    HEADER true,
-    DELIMITER ','
-);
+CREATE OR REPLACE PROCEDURE bronze.load_bronze_layer()
+LANGUAGE plpgsql
+AS $$
+BEGIN
 
-TRUNCATE TABLE bronze.erp_px_cat_g1v2;
-COPY bronze.erp_px_cat_g1v2
-FROM 'D:\projects\crm_erp_datawarehouse\datasets\source_erp\PX_CAT_G1V2.csv'
-WITH (
-    FORMAT csv,
-    HEADER true,
-    DELIMITER ','
-);
+    RAISE NOTICE 'Iniciando o carregamento dos dados na camada bronze...';
+
+    TRUNCATE TABLE bronze.crm_cust_info;
+    COPY bronze.crm_cust_info
+    FROM 'D:\projects\crm_erp_datawarehouse\datasets\source_crm\cust_info.csv'
+    WITH (
+        FORMAT csv,
+        HEADER true,
+        DELIMITER ','
+    );
+    TRUNCATE TABLE bronze.crm_prd_info;
+    COPY bronze.crm_prd_info
+    FROM 'D:\projects\crm_erp_datawarehouse\datasets\source_crm\prd_info.csv'
+    WITH (
+        FORMAT csv,
+        HEADER true,
+        DELIMITER ','
+    );
+    TRUNCATE TABLE bronze.crm_sales_details;
+    COPY bronze.crm_sales_details
+    FROM 'D:\projects\crm_erp_datawarehouse\datasets\source_crm\sales_details.csv'
+    WITH (
+        FORMAT csv,
+        HEADER true,
+        DELIMITER ','
+    );
+    TRUNCATE TABLE bronze.erp_cust_az12;
+    COPY bronze.erp_cust_az12
+    FROM 'D:\projects\crm_erp_datawarehouse\datasets\source_erp\CUST_AZ12.csv'
+    WITH (
+        FORMAT csv,
+        HEADER true,
+        DELIMITER ','
+    );
+    TRUNCATE TABLE bronze.erp_loc_a101;
+    COPY bronze.erp_loc_a101
+    FROM 'D:\projects\crm_erp_datawarehouse\datasets\source_erp\LOC_A101.csv'
+    WITH (
+        FORMAT csv,
+        HEADER true,
+        DELIMITER ','
+    );
+
+    TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+    COPY bronze.erp_px_cat_g1v2
+    FROM 'D:\projects\crm_erp_datawarehouse\datasets\source_erp\PX_CAT_G1V2.csv'
+    WITH (
+        FORMAT csv,
+        HEADER true,
+        DELIMITER ','
+    );
+
+    RAISE NOTICE 'Carregamento dos dados na camada bronze concluído com sucesso.';
+EXCEPTION
+    WHEN OTHERS THEN
+        RAISE WARNING 'Um erro ocorreu ao carregar dados na camada bronze: %', SQLERRM;
+END;
+$$;
