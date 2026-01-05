fffc93e1a6812951c204c0c6a597f487b9d42ab8
EdivanCarvalho1 <edivanjuniorfreitas@gmail.com>
2026-01-05 16:54:51 -0300

    add: database eda

--- sql/exploration/01_explore_database.sql
+++ sql/exploration/01_explore_database.sql
@@ -1,3 +1,9 @@
--- Encontrar todos os objetos no banco de dados
+-- Encontrar todos os schemas no banco de dados
+SELECT schema_name
+FROM information_schema.schemata;
 
-SELECT * FROM
+-- Encontrar todas as tabelas e views em todos os schemas
+SELECT table_schema, table_name, table_type
+FROM information_schema.tables
+WHERE table_schema NOT IN ('pg_catalog', 'information_schema')
+ORDER BY table_schema, table_name;
